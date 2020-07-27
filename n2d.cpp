c894808f76d2aabd5cda47fa020782d035a22244
Patrik Roth <patrik.roth@gmx.de>
2020-07-27 10:26:33 +0200

    Ubenennungen m_coordX --> m_coordXatLinksUnten, m_coordListY --> m_coordListYatRechtsUnten

--- n2d.cpp
+++ n2d.cpp
@@ -44,8 +44,8 @@ n2D::n2D(QList<QLineSeries *> listLineSeries)
 	//![2]
 
 	// Add few series & Kopie der Werte erstellen
-	m_coordX = new QGraphicsSimpleTextItem(m_chart);
-	m_coordX->setPos(m_chart->size().width()/2, m_chart->size().height());
+	m_coordXatLinksUnten = new QGraphicsSimpleTextItem(m_chart);
+	m_coordXatLinksUnten->setPos(m_chart->size().width()/2, m_chart->size().height());
 	m_coordXatGraf = new QGraphicsSimpleTextItem(m_chart);
 
 	int n = 0;
@@ -62,7 +62,7 @@ n2D::n2D(QList<QLineSeries *> listLineSeries)
 		series->setName(QString("%2 (%1)").arg(n).arg(ls->name()));
 		scatSer->setName(QString("(%1)").arg(n));
 		addSeries(series, scatSer);
-		m_coordListY.append(new QGraphicsSimpleTextItem(m_chart));
+		m_coordListYatRechtsUnten.append(new QGraphicsSimpleTextItem(m_chart));
 		m_coordListYatGraf.append(new QGraphicsSimpleTextItem(m_chart));
 	}
 
@@ -74,7 +74,7 @@ n2D::n2D(QList<QLineSeries *> listLineSeries)
 	m_xHilfsLinie->setPen(pen);
 	m_yHilfsLinie->setPen(pen);
 	pen.setWidthF(0.5);
-	m_coordX->setPen(pen);
+	m_coordXatLinksUnten->setPen(pen);
 	m_coordXatGraf->setPen(pen);
 
 	connectMarkers();
@@ -205,16 +205,16 @@ void n2D::setKreuzMitXYWerten(QPoint position, QString richtung)
 
 	/// xyWerte als Text unten anzeigen:
 	// x:
-	m_coordX->setPos(breite, hoehe);
-	m_coordX->setText(QString("X:%1").arg(vecExact.at(index).x()));
+	m_coordXatLinksUnten->setPos(breite, hoehe);
+	m_coordXatLinksUnten->setText(QString("X:%1").arg(vecExact.at(index).x()));
 	//m_coordXatGraf->setPos(breite, hoehe); // --> wird beim Kreuz gemacht
 	m_coordXatGraf->setText(QString("|%1").arg(vecExact.at(index).x()));
 
 	// y:
 	int rechtsVersatz = 0;
-	for (int n = m_coordListY.count()-1; n+1 ; --n) {
+	for (int n = m_coordListYatRechtsUnten.count()-1; n+1 ; --n) {
 		int versatz = rechtsVersatz*80+10; // Textabstand
-		m_coordListY.at(n)->setPos(versatz, hoehe);
+		m_coordListYatRechtsUnten.at(n)->setPos(versatz, hoehe);
 
 		QLineSeries *serie = m_series.at(n);
 		QPointF wert = serie->pointsVector().at(index);
@@ -224,15 +224,15 @@ void n2D::setKreuzMitXYWerten(QPoint position, QString richtung)
 
 		if (serie->isVisible() | m_scatSer.at(n)->isVisible()) {
 			++rechtsVersatz;
-			m_coordListY.at(n)->setPen(serie->pen());
-			m_coordListY.at(n)->setText(QString("%1:%2").arg(n+1).arg(
+			m_coordListYatRechtsUnten.at(n)->setPen(serie->pen());
+			m_coordListYatRechtsUnten.at(n)->setText(QString("%1:%2").arg(n+1).arg(
 				serie->pointsVector().at(index).y(), 0, 'E', 3)); // yWert ausgeben --> -1.234E+01
 			m_coordListYatGraf.at(n)->setPen(serie->pen());
 			m_coordListYatGraf.at(n)->setText(QString("_%1").arg(
 				serie->pointsVector().at(index).y(), 0, 'E', 3)); // yWert ausgeben --> -1.234E+01
 		}
 		else {
-			m_coordListY.at(n)->setText("");
+			m_coordListYatRechtsUnten.at(n)->setText("");
 			m_coordListYatGraf.at(n)->setText("");
 		}
 	}
@@ -389,8 +389,8 @@ void n2D::removeHiddenSeries()
 			m_chart->removeSeries(m_scatSer.at(i));
 			m_series.removeOne(m_series.at(i));
 			m_scatSer.removeOne(m_scatSer.at(i));
-			m_coordListY.last()->setText("");
-			m_coordListY.removeLast();
+			m_coordListYatRechtsUnten.last()->setText("");
+			m_coordListYatRechtsUnten.removeLast();
 			m_coordListYatGraf.last()->setText("");
 			m_coordListYatGraf.removeLast();
 		}
